366551914419243febbcf86bb50a9e3f64beb98a
NiclasLundstrom <lille_niclas@hotmail.com>
2021-02-01 12:01:19 +0100

    la till filer

---
+++ precache-manifest.d72bb9435074397b63ff273674c6a7ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7d29c34b88b208f726",
+    "url": "/css/app.260291aa.css"
+  },
+  {
+    "revision": "fe8aeff658bdfdd661ec",
+    "url": "/css/chunk-vendors.01c183df.css"
+  },
+  {
+    "revision": "8657bbf5f0f39c293da72ba41ad4ca64",
+    "url": "/img/Group 11.8657bbf5.png"
+  },
+  {
+    "revision": "66f0c6e576a21d0ba12fecc81c989657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f7d29c34b88b208f726",
+    "url": "/js/app.ae7bd608.js"
+  },
+  {
+    "revision": "fe8aeff658bdfdd661ec",
+    "url": "/js/chunk-vendors.1d0cf9a9.js"
+  },
+  {
+    "revision": "78f3d3c78a44979f7e298ee814e0c4fa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,37 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.d72bb9435074397b63ff273674c6a7ca.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "project"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute("https://avancera.app/cities/", new workbox.strategies.NetworkFirst({ "networkTimeoutSeconds":5, plugins: [] }), 'GET');
+workbox.routing.registerRoute("https://cors-anywhere.herokuapp.com/http://yesno.wtf/api/", new workbox.strategies.NetworkFirst({ "networkTimeoutSeconds":5, plugins: [] }), 'GET');
